fa8cf4df2d3a231b1fa920291004b94085904f7f
kabingusam <kabingusammy@gmail.com>
2022-05-31 00:54:30 +0300

    update

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,3 @@
-from asyncore import write
-from sys import stderr
-
-
-write("kabingu")
+#!/usr/bin/python3
+import sys
+sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
