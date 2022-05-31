12de1aa824dc2fcb2da45ce4a815345fbfd7898a
sammy <80510478+kabingusam@users.noreply.github.com>
2022-05-31 13:30:29 +0300

    update

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,32 +1,30 @@
+#include <stdlib.h>
+#include "lists.h"
 /**
- * Author : Kabingu Sammy.
+ * check_cycle - Checks if a singly-linked list contains a cycle.
+ * @list: A singly-linked list.
  *
+ * Return: If there is no cycle - 0.
+ *         If there is a cycle - 1.
  */
- #include <stdlib.h>
- #include "lists.h"
-
- /**
-  * check_cycle : checks wether a linked list is cyclic.
-  * 
-  * return 0 : if list is not cyclic.
-  * return 1 : if list is cyclic.
-  */
 int check_cycle(listint_t *list)
 {
-    listint_t *kabi, *sami;
-    
-    if(list == NULL || list->next == NULL)
-        return(0);
-    
-    kabi = kabi->next;
-    sami = kabi->next->next;
+	listint_t *kabi, *sami;
 
-    while(kabi && sami && sami->next)
-    {
-        if(kabi = sami)
-            return(1);
-        kabi = kabi->next;
-        sami = kabi->next->next;
-    }
-    return(0);
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	kabi = list->next;
+	sami = list->next->next;
+
+	while (kabi && sami && sami->next)
+	{
+		if (kabi == sami)
+			return (1);
+
+		kabi = kabi->next;
+		sami = sami->next->next;
+	}
+
+	return (0);
 }
--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-number , street = 98 ,"Battery street"
-print(f"{number} {street}\n")
+number = 98
+print("{:d} Battery street".format(number))
