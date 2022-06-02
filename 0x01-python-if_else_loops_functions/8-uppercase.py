a30598196dc1255095fd4679f0eff1c8071f1ce1
sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-02 14:16:27 +0300

    uppercase task completion

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+def uppercase(str):
+    #chr() function returns the character that represents the specified unicode.
+    #ASCII value of the uppercase alphabet is from 65 to 90.
+    for c in str:
+        if ord(c) => 97 and ord(c) <= 122:
+            c =  char(ord(c) - 32)
+            print(f"{}".format(c), end ="")
+            print("")
