1f2a031b33a8cadd8dc071d96f39c86694058953
sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-02 14:45:11 +0300

    fizzbuzz task completion

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz():
+    for num in range (1,101):
+        # multiple of 3  print Fizz
+        # multiple of 5 print Buzz
+        # multiple of both 3 and 5 print FizzBuzz
+        if num % 3 == 0 and num % 5 == 0:
+            print(("FizzBuzz"),end="")
+        elif num % 3 == 0:
+            print(("Fizz"),end="")
+        elif num % 5 == 0:
+            print(("Buzz"),end="")
+        else:
+            print("{}".format(num),end="")
