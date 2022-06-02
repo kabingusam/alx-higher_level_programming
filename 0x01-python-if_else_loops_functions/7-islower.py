4434eaff70d4172474d74194357f3cfa8eb1b2e7
sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-02 14:08:37 +0300

    task7 upadate

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+def islower(c):
+    # ord() function returns the number representing the unicode code of a specified character.
+    # lowercase c range :from 97 to 122
+    if ord(c) => 97  and ord(c) <= 122:
+        return true
+    else:
+        return false
