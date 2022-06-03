b928b2ba1d6610508e3e85fd9e47535eb3024358
sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-03 05:07:48 -0700

    importing a variable

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,1 +1,6 @@
+#!/usr/bin/python3
 if __name__ == "__main__":
+    '''import variable from variable_load_5.py a and print its value'''
+    from variable_load_5.py import a
+
+    print("{}".format(a))
