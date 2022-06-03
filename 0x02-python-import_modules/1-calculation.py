772848ac7f63666183b32791e1697792b011bd9e
sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-03 02:44:51 -0700

    calculator task complete

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+if _name_ == "_main_":
+    '''normal math functions are to be done: add,subtract,muiltply and divide'''
+    from calculator_1.py import add,sub,mul,div
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a,b, add(a,b)))
+    print("{} - {} = {}".format(a,b, sub(a,b)))
+    print("{} * {} = {}".format(a,b, mul(a,b)))
+    print("{} / {} = {}".format(a,b, div(a,b)))
