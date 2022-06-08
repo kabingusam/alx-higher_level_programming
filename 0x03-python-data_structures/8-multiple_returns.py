02c6b60839db1ec1ee741c884de1047d4adefcd1
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-08 13:41:14 +0300

    tuple task 8 complete

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    ''' return a tuple with the length of a string and its first character'''
+    length = len(sentence)
+    if length > 0:
+        first_char == sentence[0]
+    else:
+        first_char == "None"
+    tup = length, first_char
+    return(tup)
