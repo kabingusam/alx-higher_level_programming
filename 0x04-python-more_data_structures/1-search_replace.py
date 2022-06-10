b7d03be141869dbbc339cab636b442507d7e6e85
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-10 15:55:59 +0300

    task 1 completion

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,6 @@
-
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    '''replaces all occurrences of an element by another in a new list.'''
+    search == n
+    replace == y
+    return(list(map(lambda x: y if  x == n else x,my_list)))
