4dcd7eb3283306c8c24f5e38915fe70c040e8367
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-10 16:08:14 +0300

    task2 completion

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,5 @@
-
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    '''add all unique integers in a list (only once for each integer).'''
+    # set() - eliminates all duplicates
+    return(sum(set(my_list)))
