9f863e48714f56c0e13e933921fb6c07e385c63c
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-11 22:23:44 +1100

    task 10 completion

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,8 @@
-
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    largest_num = a_dictionary[0]
+    if a_dictionary = {}:
+        return None
+    if x > largest_num:
+        largest_num = x
+    return(x)
