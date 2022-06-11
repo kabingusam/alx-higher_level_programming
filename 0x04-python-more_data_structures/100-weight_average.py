b63b0f30daa871b6f4416ac263597e797d6913d4
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-11 22:38:38 +1100

    task completion

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,10 @@
-
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list = [] or my_list is None:
+        return(0)
+    result = 0
+    result1 = 0
+    for x,y in my_list:
+        result += x * y
+        reult2 += y
+    return(result / result1)
