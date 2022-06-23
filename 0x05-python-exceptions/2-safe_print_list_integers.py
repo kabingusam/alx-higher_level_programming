4f2d946f34d23c9a8a2f4ddf11e4b9ded8897473
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-23 05:45:13 -0700

    task correction and completion

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+#num : real number of elements printed
+def safe_print_list_integers(my_list=[], x=0):
+    num = 0;
+    for i in range(x):
+    try:
+            print("{:d}".format(my_list[i]),end='')
+            num += 1
+    except (ValueError,TypeError):
+        num += 1
+        continue
+    print("")
+    return(num)
