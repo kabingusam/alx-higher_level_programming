deb1f648adfa167373dd810a0deddb51a5026754
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-25 14:04:03 +0300

    task 3  update and completion

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,4 +2,19 @@
 "Define a class square"
 
 
-class S
+class Square:
+    def __init__(self,size = 0):
+        """Initialize the square
+
+        Args: size (int) = size of the new square.
+
+        """
+        if not isinstance(size,int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """initialize the area of the square"""
+        return(self.__size * self.__size)
