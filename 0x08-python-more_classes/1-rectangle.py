ef9859805320c647233346c3150fbdc06007a5b0
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-07-07 08:47:37 +0300

    task1 update

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!usr/bin/python3
+'''
+Defines a rectangle
+'''
+class Rectangle:
+    '''Represents a reactangle'''
+    def __init__(self,width = 0,height = 0):
+
+        ''' Initialize a new rectangle 
+        Args: 
+        width(int) : width of the rectangle
+        height(int) : height of the rectangle
+        
+        '''
+        self.width = width
+        self.height = height
+    @property
+    def width(self):
+        """get the width of the reactangle."""
+        return self.__width
+    @width.setter
+    def width(self,value):
+        if not isinstance (value,int):
+            raise TypeError("width must be an integer")
+        if value < 0 :
+            raise ValueError("width must be >= 0")
+        self.__width
+    @property
+    def height(self):
+        """get the height of the rectangle"""
+        return self.__height
+    
+    @height.setter
+    def height(self,value):
+        if not isinstance(value,int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
