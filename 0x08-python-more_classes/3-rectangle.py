a05a56168bd7c7822c6c8188928e16fdf7a3a1d5
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-07-07 09:47:44 +0300

    task3 update

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,1 +1,56 @@
 #!usr/bin/python3
+'''Defines a rectangle'''
+class Rectangle:
+    '''Repesents a rectangle'''
+    def __init__(self,width = 0, height = 0):
+        '''
+        Initialize an new Rectangle
+        Args:
+        width(int) :  width of the rectangle
+        height(int) : height of the rectangle
+
+        '''
+        self.width = width
+        self.height = height
+        @property
+        def width(self):
+            '''get the width of the rectangle'''
+            return self.__width
+        
+        @width.setter
+        def width(self,value):
+            if not isinstance (value,int):
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+
+        @property
+        def height(self,value):
+            if not isinstance(value,int):
+                raise TypeError("height must be an intrger")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__height = value
+        
+        def area():
+            '''Return the area of the rectangle'''
+            return(self.__width * self.__height)
+        
+        def perimeter():
+            '''Return the perimeter of the rectangle'''
+            if(self.__width == 0 or self.__height == 0):
+                return(0)
+            return((self.__height * 2) + (self.__width * 2))
+
+        def __str__(self):
+            '''Return the printable representation of the rectangle'''
+            if(self.__width == 0 or self.__height == 0):
+                return("")
+
+        Rectangle []
+        for i in range(self.__height):
+            [Rectangle.append('#') for j in range self.__width]
+            if i != self.__height - 1:
+                Rectangle.append('\n')
+                return("".join(Rectangle))
