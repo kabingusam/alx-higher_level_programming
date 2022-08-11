55d940d9828f33d943718ed1faaaaf66d31eca01
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-08-11 14:31:40 +0300

    update

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,5 @@
--- top 3 of cities with the highest temperature 
--- during July and August ordered by temperature (descending)
+-- Top 3 of cities with the highest temperature 
+-- During July and August ordered by temperature (descending)
 SELECT `city`, AVG(`value`) AS `avg_tmp`
 FROM `temperatures`
 WHERE `month` = 7 OR `month` = 8
