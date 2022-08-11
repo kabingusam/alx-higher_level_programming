f25f70f8b05d054024fcab4c50da5d968b069975
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-08-11 12:14:49 +0300

    update on minor changes

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,10 +2,10 @@
 -- User should have all privileges
 pass :user_0d_1_pwd
 CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
+    IF NOT EXISTS `user_0d_1`@`localhost`
+    IDENTIFIED BY `user_0d_1_pwd`;
 GRANT ALL PRIVILEGES
     ON *.*
-    TO USER 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
+    TO USER `user_0d_1`@`localhost`
+    IDENTIFIED BY `user_0d_1_pwd`;
 FLUSH PRIVILEGES;
