c413fec1cc67df417ba576a2207b9c23be8399d7
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-09-07 14:47:52 +0300

    update

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,6 +4,6 @@ module.exports = class Rectangle {
         if(w > 0 && h > 0){ [this.width, this.heigth] = [w, h]; }
     }
     print(){
-        for( i = 0; i < this.heigth; i ++) console.log('X'.repeat(this.width));
+        for(let i = 0; i < this.heigth; i ++) console.log('X'.repeat(this.width));
     }
 };
